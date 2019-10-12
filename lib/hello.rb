259faae8c5eda64f8c890ba638eca5c9c38dd8f7
RokhayaN <rokhayamuslima1@gmail.com>
2019-10-12 18:24:28 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,5 +1,9 @@
-def hello_t
-
+def hello_t (["Tim", "Tom", "Jim."])
+  names = ["Tim", "Tom", "Jim"]
+  hello_t(names)do|name|
+  if name.start_with? ("T")
+    puts "Hi,#{name}"
+  end 
 end
 
 # call your method here!
